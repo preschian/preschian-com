9b9f88b4a1cb16076c87a4d8fa461f48b51bd827
Preschian Febryantara <preschian27@gmail.com>
2017-02-16 17:32:22 +0700

    some changes with api.js

--- utils/api.js
+++ utils/api.js
@@ -1,15 +1,27 @@
-import database from './firebase'
+import firebase from 'firebase'
+
+try {
+  firebase.initializeApp({
+    databaseURL: 'https://preschian-com.firebaseio.com'
+  })
+} catch(err) {
+  if (!/already exists/.test(err.message)) {
+    console.error('Firebase initialization error', err.stack)
+  }
+}
+
+const database = firebase.database()
 
 export const GetPosts = (page = 1, eachPage = 4) => {
   const start = (eachPage * (page - 1) + 1)
 
-  return database.ref('/').orderByChild('order').startAt(start).limitToFirst(eachPage).once('value')
+  return database.ref('/post').orderByChild('order').startAt(start).limitToFirst(eachPage).once('value')
 }
 
 export const GetPostDetail = (slug) => {
-  return database.ref('/').orderByChild('slug').equalTo(slug).once('value')
+  return database.ref('/post').orderByChild('slug').equalTo(slug).once('value')
 }
 
 export const GetTotalPosts = () => {
-  return database.ref('/').orderByChild('order').limitToLast(1).once('value')
+  return database.ref('/post').orderByChild('order').limitToLast(1).once('value')
 }
